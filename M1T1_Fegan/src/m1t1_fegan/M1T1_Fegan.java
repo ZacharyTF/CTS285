96c3ea37aed4a9067735ac279baadd1cea4f2d15
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2021-08-18 09:57:56 -0400

    Add files via upload

---
+++ M1T1_Fegan/src/m1t1_fegan/M1T1_Fegan.java
@@ -0,0 +1,17 @@
+/*
+ * CTS 285
+ * M1T1
+ * Zachary Fegan
+ * 08-16-2021
+ */
+package m1t1_fegan;
+
+public class M1T1_Fegan {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Zachary Fegan.");
+        System.out.println("I am currently taking/studying C (sharp) and Java this semester.");
+        System.out.println("I like to sketch, play video games, and I volunteer at my local church.");
+    }
+    
+}
